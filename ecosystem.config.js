debb3681c1a22f110f75ae201dbec71f41198ac0
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-05-17 12:38:59 +0200

    remove deploy script for now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,15 +6,4 @@ module.exports = {
       args: "start",
     },
   ],
-  deploy: {
-    production: {
-      "user": "hauke",
-      "host": "haukeschnau.de",
-      "ref": "origin/main",
-      "repo": "git@github.com:HaukeSchnau/ClassMate.git",
-      "path": "/home/hauke/apps/ClassMate",
-      "post-setup": "scripts/setup && ni && nr db:generate && nr db:push",
-      "post-deploy": "ni && nr build && pm2 startOrRestart ecosystem.config.js"
-    },
-  },
 };
