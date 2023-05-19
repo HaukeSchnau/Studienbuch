571918d86727fd512d796eb704b16dd7491fedde
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-05-19 10:24:38 +0200

    add marketing page

--- apps/nextjs/tailwind.config.cjs
+++ apps/nextjs/tailwind.config.cjs
@@ -3,6 +3,14 @@ const config = {
   content: ["./src/**/*.tsx"],
   // @ts-ignore
   presets: [require("@acme/tailwind-config")],
+  theme: {
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
+    colors: {
+      green: "#098A00",
+    },
+  },
 };
 
 module.exports = config;
