0f6936d017080a9ce9a4c1e8a36bed80f5bfa212
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-06-02 19:56:04 +0200

    ran flutterfire configure

--- apps/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import package_info_plus
 import path_provider_foundation
 import sentry_flutter
@@ -12,6 +13,7 @@ import sqlite3_flutter_libs
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
