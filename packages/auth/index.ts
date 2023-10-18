f8cae0d5ec2ce0013b73765e423478d183d8a7f6
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-10-18 11:08:53 +0200

    upgrade auth

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,3 +1,42 @@
-export { authOptions } from "./src/auth-options";
-export { getServerSession } from "./src/get-session";
-export type { Session } from "next-auth";
+import { z } from "zod";
+
+import { db } from "@acme/db";
+import { getCookies } from "@acme/common/src/cookies";
+
+export interface Session {
+  token: string;
+  expires: Date;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    image: string | null;
+  } | null;
+}
+
+const cookieSchema = z.object({
+  session: z.string().optional(),
+});
+
+export const authHandler = (
+  requestHandler: (req: Request, session: Session | null) => Promise<Response>,
+) => {
+  return async (req: Request) => {
+    const sessionToken =
+      req.headers.get("x-session") ??
+      getCookies(req.headers, cookieSchema)?.session;
+
+    const session = sessionToken
+      ? await db.session.findFirst({
+          where: {
+            token: sessionToken,
+          },
+          include: {
+            user: true,
+          },
+        })
+      : null;
+
+    return requestHandler(req, session);
+  };
+};
