345d8627bca6f2ffd6b4bfe330bfe4f82ee90aa1
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-05 22:37:09 +0100

    Add Home link to Header component

--- apps/nextjs/src/components/layout/Header.tsx
+++ apps/nextjs/src/components/layout/Header.tsx
@@ -4,9 +4,10 @@ import { NavLink } from "./NavLink";
 export const Header = () => {
   return (
     <nav className="mx-auto flex w-min gap-8 rounded-3xl bg-primary-400 px-8 py-4 text-white shadow-md">
+      <NavLink href="/">Home</NavLink>
       <NavLink href="/roadmap">Roadmap</NavLink>
-      <NavLink href="/blog">Blog</NavLink>
-      <NavLink href="/about">About</NavLink>
+      {/* <NavLink href="/blog">Blog</NavLink>
+      <NavLink href="/about">About</NavLink> */}
       <NavLink href="/admin">{isLoggedIn() ? "Admin" : "Login"}</NavLink>
     </nav>
   );
