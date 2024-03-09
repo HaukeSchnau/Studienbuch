af4215771c8dac7900dd89303fb316b4f7dcf0f1
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-09 14:21:50 +0100

    Reduce width of sidebar

--- apps/nextjs/src/app/admin/layout.tsx
+++ apps/nextjs/src/app/admin/layout.tsx
@@ -19,7 +19,7 @@ export default function AdminLayout({ children }: Props) {
 
   return (
     <div className="bg-main-blob flex h-screen overflow-hidden bg-offwhite bg-contain bg-no-repeat">
-      <div className="basis-1/5 rounded-r-3xl bg-white p-4 shadow-md">
+      <div className="basis-1/6 rounded-r-3xl bg-white p-4 shadow-md">
         <div className="mx-auto w-4/6 py-12">
           <img
             src="/assets/icon.png"
