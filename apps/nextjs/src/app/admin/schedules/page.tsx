fb78900ea1d03fc93d4f1ff94f78c0970a848f16
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-11 22:43:16 +0100

    Add Import Schedule Modal

--- apps/nextjs/src/app/admin/schedules/page.tsx
+++ apps/nextjs/src/app/admin/schedules/page.tsx
@@ -1,16 +1,30 @@
 "use client";
 
+import { useState } from "react";
+
+import { Button } from "~/components/form/Button";
 import { PageHeading } from "~/components/layout/PageHeading";
-import Timetable from "~/features/timetable/Timetable";
+import { TimetableManager } from "~/features/timetable/TimetableManager";
 import { useSelectedYear } from "~/features/yearSelect/selectedYearStore";
+import { ImportScheduleModal } from "./componets/ImportScheduleModal";
 
 export default function SchedulesPage() {
   const { selectedYear } = useSelectedYear();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="flex flex-col gap-4">
-      <PageHeading color="white">Stundenpläne</PageHeading>
-      {selectedYear && <Timetable yearId={selectedYear.id} />}
+      <div className="flex items-center justify-between">
+        <PageHeading color="white">Stundenpläne</PageHeading>
+        <Button onClick={() => setIsModalOpen(true)}>Importieren</Button>
+      </div>
+      {selectedYear && <TimetableManager yearId={selectedYear.id} />}
+
+      <ImportScheduleModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </div>
   );
 }
