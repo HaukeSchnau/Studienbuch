af152b15ba2cbbcff045e961164031ffa2e162f3
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-11 07:18:23 +0100

    Add useSafeParams hook

---
+++ apps/nextjs/src/infrastructure/hooks/useSafeParams.ts
@@ -0,0 +1,7 @@
+import type { ZodSchema } from "zod";
+import { useParams } from "next/navigation";
+    
+export const useSafeParams = <T>(schema: ZodSchema<T>) => {
+  const params = useParams();
+  return schema.parse(params);
+};
