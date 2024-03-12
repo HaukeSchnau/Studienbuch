519d8dce5670553c4364576515a8b16b596b3ace
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-12 19:47:40 +0100

    Add class list page

--- apps/nextjs/src/app/admin/classes/components/ClassList.tsx
+++ apps/nextjs/src/app/admin/classes/components/ClassList.tsx
@@ -1,3 +1,6 @@
+import { Card, CardHeading } from "~/components/layout/Card";
+import { Grid } from "~/components/layout/Grid";
+import { LoadingIndicator } from "~/components/layout/LoadingIndicator";
 import { api } from "~/infrastructure/trpc/react";
 
 interface Props {
@@ -5,6 +8,25 @@ interface Props {
 }
 
 export const ClassList = ({ yearId }: Props) => {
-  const courses = api.courses.list.useQuery({ yearId });
+  const classes = api.classes.list.useQuery({ yearId });
 
+  if (classes.status === "pending") {
+    return <LoadingIndicator />;
+  }
+
+  if (classes.status === "error") {
+    return <div>Error: {classes.error.message}</div>;
+  }
+
+  return (
+    <Grid
+      data={classes.data}
+      renderItem={(clazz) => (
+        <Card key={clazz.id}>
+          <CardHeading>{clazz.identifierInYear}</CardHeading>
+          <p>{clazz.courses.length} Kurse</p>
+        </Card>
+      )}
+    />
+  );
 };
