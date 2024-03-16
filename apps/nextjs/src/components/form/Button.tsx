56e1b7eb106cd41d6ae87eb279c7adbe5f29f01a
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-16 14:30:59 +0100

    Add "yellow" variant to Button component

--- apps/nextjs/src/components/form/Button.tsx
+++ apps/nextjs/src/components/form/Button.tsx
@@ -5,7 +5,7 @@ import clsx from "clsx";
 type ButtonProps = {
   children: ReactNode;
   className?: string;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "yellow";
 } & (
   | {
       href: string;
@@ -28,6 +28,8 @@ export function Button({
     variant === "primary" &&
       "border-b-blue-sec border-t-blue bg-blue hover:border-t-blue-sec hover:bg-blue-sec text-white",
     variant === "secondary" && "border-transparent hover:bg-grey text-darkgrey",
+    variant === "yellow" &&
+      "border-b-yellow-sec border-t-yellow bg-yellow hover:border-t-yellow-sec hover:bg-yellow-sec text-white",
     className,
   );
 
