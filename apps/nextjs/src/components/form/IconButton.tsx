c7cadf1a05a39dce836e1f027f3fc772d3722149
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-18 18:43:20 +0100

    Support href in IconButton

--- apps/nextjs/src/components/form/IconButton.tsx
+++ apps/nextjs/src/components/form/IconButton.tsx
@@ -1,29 +1,39 @@
+import Link from "next/link";
 import clsx from "clsx";
 
 import type { IconName } from "../layout/icon";
 
+type Props = {
+  icon: IconName;
+  className?: string;
+  size?: "sm" | "md" | "lg";
+} & (
+  | {
+      onClick: () => void;
+      href?: never;
+    }
+  | {
+      href: string;
+      onClick?: never;
+    }
+);
+
 export const IconButton = ({
   icon,
   onClick,
+  href,
   className,
   size = "md",
-}: {
-  icon: IconName;
-  onClick: () => void;
-  className?: string;
-  size?: "sm" | "md" | "lg";
-}) => (
-  <button
-    type="button"
-    onClick={onClick}
-    className={clsx(
-      "rounded-full bg-transparent transition-colors hover:bg-black-80",
-      className,
-      size === "sm" && "h-6 w-6",
-      size === "md" && "h-8 w-8",
-      size === "lg" && "h-12 w-12",
-    )}
-  >
+}: Props) => {
+  const classes = clsx(
+    "rounded-full bg-transparent transition-colors hover:bg-black-80 grid place-items-center",
+    className,
+    size === "sm" && "h-6 w-6",
+    size === "md" && "h-8 w-8",
+    size === "lg" && "h-12 w-12",
+  );
+
+  const content = (
     <i
       className={clsx(
         size === "sm" && "text-sm",
@@ -33,5 +43,19 @@ export const IconButton = ({
     >
       {icon}
     </i>
-  </button>
-);
+  );
+
+  if (onClick) {
+    return (
+      <button type="button" onClick={onClick} className={classes}>
+        {content}
+      </button>
+    );
+  }
+
+  return (
+    <Link href={href} className={classes}>
+      {content}
+    </Link>
+  );
+};
