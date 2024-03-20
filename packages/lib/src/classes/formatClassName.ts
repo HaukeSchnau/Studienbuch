fa5a8f3e6b89690c74c62f42256235469663acdd
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-03-20 20:28:34 +0100

    Fix formatClassName

--- packages/lib/src/classes/formatClassName.ts
+++ packages/lib/src/classes/formatClassName.ts
@@ -6,14 +6,15 @@ export const formatClassName = (
   clazz: Pick<Class, "identifierInYear">,
   year: Pick<Year, "name" | "startYear" | "graduationYear">,
 ) => {
-  if (!clazz.identifierInYear) {
-    return year.name;
-  }
-
   if (!isYearActive(year)) {
     return `${year.name} ${clazz.identifierInYear}`.trim();
   }
 
   const currentYear = getCurrentYearNum(year);
+
+  if (!clazz.identifierInYear) {
+    return `${currentYear}`;
+  }
+
   return `${currentYear}.${clazz.identifierInYear}`;
 };
