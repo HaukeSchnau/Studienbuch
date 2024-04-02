580f38f57ce002254faafdf38dad847389301b33
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-04-02 13:44:48 +0200

    re-add error handling

--- apps/flutter/lib/infrastructure/hooks/use_network_result.dart
+++ apps/flutter/lib/infrastructure/hooks/use_network_result.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 T? useNetworkResult<T>(Future<T> Function() future,
@@ -6,15 +7,15 @@ T? useNetworkResult<T>(Future<T> Function() future,
   final result = useState<T?>(null);
   useEffect(() {
     () async {
-      // try {
-      result.value = await future();
-      // } on Error catch (e) {
-      //   debugPrint(e.toString());
-      //   onError(e, e.stackTrace);
-      // } catch (e) {
-      //   debugPrint(e.toString());
-      //   onError(e, null);
-      // }
+      try {
+        result.value = await future();
+      } on Error catch (e) {
+        debugPrint(e.toString());
+        onError(e, e.stackTrace);
+      } catch (e) {
+        debugPrint(e.toString());
+        onError(e, null);
+      }
     }();
     return null;
   }, keys);
