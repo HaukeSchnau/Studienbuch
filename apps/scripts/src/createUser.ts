d044e44ab4cbdad0fe0bf38efed22721dda08d46
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-04-03 19:04:21 +0200

    Delete unused scripts

--- apps/scripts/src/createUser.ts
+++
@@ -1,8 +0,0 @@
-import { createUser } from "@schnau/lib-server";
-
-if (!process.argv[3] || !process.argv[4] || !process.argv[5]) {
-  console.log("Usage: createUser <name> <email> <password>");
-  process.exit(1);
-}
-
-void createUser(process.argv[3], process.argv[4], process.argv[5]);
--- apps/scripts/src/seed/index.ts
+++
@@ -1,7 +0,0 @@
-import { generateLicenses } from "./generateLicenses";
-
-export const seed = async () => {
-  await generateLicenses(100);
-};
-
-void seed();
