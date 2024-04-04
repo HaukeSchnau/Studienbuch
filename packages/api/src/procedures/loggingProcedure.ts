9e54c75e7319712fddcc62e6b2ff23fe5abf7079
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-04-04 22:55:15 +0200

    Update trpc log message

--- packages/api/src/procedures/loggingProcedure.ts
+++ packages/api/src/procedures/loggingProcedure.ts
@@ -8,7 +8,7 @@ export const logger = t.middleware(async ({ ctx, next, path, type }) => {
   const durationMs = performance.now() - start;
   const meta = { path: path, type: type, durationMs };
 
-  ctx.log.info("procedure", meta);
+  ctx.log.info("tRPC", meta);
   await ctx.log.flush();
 
   return result;
