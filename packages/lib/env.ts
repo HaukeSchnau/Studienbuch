518fe8f72a6a6d62d3444c6303677723da9da90e
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-04-18 18:31:49 +0200

    Remove env from lib

--- packages/lib/env.ts
+++
@@ -1,8 +0,0 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-
-export const env = createEnv({
-  server: {},
-  client: {},
-  experimental__runtimeEnv: {},
-  skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
-});
