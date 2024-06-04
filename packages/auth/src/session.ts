e662feb79b9d3ab88954a086cf616f189435f614
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-06-04 14:26:18 +0200

    Update session management to drizzle

--- packages/auth/src/session.ts
+++ packages/auth/src/session.ts
@@ -1,17 +1,19 @@
 import crypto from "crypto";
 
-import { db } from "@schnau/db";
+import type { User } from "@schnau/lib";
+import { eq } from "@schnau/db";
+import { db } from "@schnau/db/client";
+import { Session } from "@schnau/db/schema";
+import { isArraySingleElement } from "@schnau/lib";
 
-import type { Session } from "./index";
+import type { Session as SessionType } from "./index";
 
 export const getSession = async (sessionToken: string) => {
-  const session = await db.session.findFirst({
-    where: {
-      token: sessionToken,
-    },
-    include: {
+  const session = await db.query.Session.findFirst({
+    with: {
       user: true,
     },
+    where: eq(Session.token, sessionToken),
   });
 
   if (!session) {
@@ -19,30 +21,28 @@ export const getSession = async (sessionToken: string) => {
   }
 
   if (session.expires < new Date()) {
-    await db.session.delete({
-      where: {
-        token: sessionToken,
-      },
-    });
+    await db.delete(Session).where(eq(Session.token, sessionToken));
     return null;
   }
 
   return session;
 };
 
-export const createSession = (user: { id: number }): Promise<Session> => {
-  return db.session.create({
-    data: {
+export const createSession = async (user: User): Promise<SessionType> => {
+  const newSession = await db
+    .insert(Session)
+    .values({
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // in 30 days
       token: crypto.randomUUID(),
-      user: {
-        connect: {
-          id: user.id,
-        },
-      },
-    },
-    include: {
-      user: true,
-    },
-  });
+      userId: user.id,
+    })
+    .returning();
+  if (!isArraySingleElement(newSession)) {
+    throw new Error("Expected exactly one session to be created");
+  }
+
+  return {
+    ...newSession[0],
+    user,
+  };
 };
