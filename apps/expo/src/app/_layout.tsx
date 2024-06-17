e2035587e5d8d359b62d2022fe5b20d9a9098a66
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-06-17 07:49:13 +0200

    Strip boilerplate from expo

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -2,29 +2,24 @@ import "@bacons/text-decoder/install";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "nativewind";
 
 import { TRPCProvider } from "~/utils/api";
 
 import "./styles.css";
 
-// This is the main layout of the app
-// It wraps your pages with the providers they need
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
   return (
     <TRPCProvider>
-      {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
-        */}
       <Stack
         screenOptions={{
           headerStyle: {
-            backgroundColor: "#f472b6",
+            backgroundColor: "#33A42B",
+          },
+          headerTitleStyle: {
+            color: "#FFFFFF",
           },
           contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
+            backgroundColor: "#FFFFFF",
           },
         }}
       />
--- tooling/tailwind/native.ts
+++ tooling/tailwind/native.ts
@@ -2,8 +2,92 @@ import type { Config } from "tailwindcss";
 
 import base from "./base";
 
+export const colors = {
+  primary: {
+    DEFAULT: "#33A42B",
+    pale: "#6DB868",
+    des: "#EEF5ED",
+
+    text: "#098A00",
+  },
+
+  accent: {
+    DEFAULT: "#3B7FD9",
+    sec: "#4d75a8",
+    pale: "#76A6E5",
+    des: "#EBF0F7",
+  },
+
+  danger: {
+    DEFAULT: "#A42B33",
+    sec: "#8A0000",
+    des: "#ECD4D6",
+  },
+
+  alert: {
+    DEFAULT: "#DCAB3C",
+    des: "#F2E9D8",
+  },
+
+  success: {
+    DEFAULT: "#33A42B",
+    des: "#EEF5ED",
+    pale: "#6DB868",
+  },
+
+  neutral: {
+    DEFAULT: "#666666",
+    sec: "#e5e5e5",
+  },
+
+  surface: "#ffffff",
+  background: "#f9f9f9",
+
+  on: {
+    primary: "#ffffff",
+    "primary-pale": "#ffffff",
+    "primary-des": "#ffffff",
+
+    accent: "#ffffff",
+    "accent-sec": "#ffffff",
+    "accent-pale": "#ffffff",
+    "accent-des": "#ffffff",
+
+    danger: "#ffffff",
+    "danger-sec": "#ffffff",
+    "danger-des": "#ffffff",
+
+    alert: "#ffffff",
+    "alert-des": "#ffffff",
+
+    success: "#ffffff",
+    "success-des": "#ffffff",
+    "success-pale": "#ffffff",
+
+    neutral: "#ffffff",
+    "neutral-sec": "#ffffff",
+
+    surface: "#000000",
+
+    background: "#000000",
+  },
+
+  white: "#FFFFFF",
+  "grey-100": "#E6E6E6",
+
+  transparent: "transparent",
+
+  black: {
+    80: "#0000000b",
+  },
+};
+
 export default {
   content: base.content,
-  presets: [base],
-  theme: {},
+  presets: [],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
 } satisfies Config;
