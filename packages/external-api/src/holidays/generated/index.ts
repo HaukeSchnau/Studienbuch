32146ab9a6291dfc5e1c8396846adbd13de848cb
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-07-09 14:21:38 +0200

    Add auto-generated holiday api code to repo

---
+++ packages/external-api/src/holidays/generated/index.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+ 
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export { HolidayDto } from './models/HolidayDto';
+
+export { HolidayWsV1ImplService } from './services/HolidayWsV1ImplService';
---
+++ packages/external-api/src/holidays/generated/models/HolidayDto.ts
@@ -0,0 +1,32 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* eslint-disable */
+export type HolidayDto = {
+    end?: string;
+    name?: string;
+    slug?: string;
+    start?: string;
+    stateCode?: HolidayDto.stateCode;
+    year?: number;
+};
+export namespace HolidayDto {
+    export enum stateCode {
+        BB = 'BB',
+        BE = 'BE',
+        BW = 'BW',
+        BY = 'BY',
+        HB = 'HB',
+        HE = 'HE',
+        HH = 'HH',
+        MV = 'MV',
+        NI = 'NI',
+        NW = 'NW',
+        RP = 'RP',
+        SH = 'SH',
+        SL = 'SL',
+        SN = 'SN',
+        ST = 'ST',
+        TH = 'TH',
+    }
+}
+
