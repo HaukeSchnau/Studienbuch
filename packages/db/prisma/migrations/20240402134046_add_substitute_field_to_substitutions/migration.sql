6f331cfdf770ea68a1ac23d87e3aefda28444e13
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-09-04 10:10:47 +0200

    Kill prisma stuff

--- packages/db/prisma/migrations/20240402134046_add_substitute_field_to_substitutions/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Substitution" ADD COLUMN     "substituteId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Substitution" ADD CONSTRAINT "Substitution_substituteId_fkey" FOREIGN KEY ("substituteId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- packages/db/prisma/migrations/20240430074638_add_state_code/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "StateCode" AS ENUM ('BB', 'BE', 'BW', 'BY', 'HB', 'HE', 'HH', 'MV', 'NI', 'NW', 'RP', 'SH', 'SL', 'SN', 'ST', 'TH');
-
--- AlterTable
-ALTER TABLE "School" ADD COLUMN     "stateCode" "StateCode" NOT NULL DEFAULT 'NI';
