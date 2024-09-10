6125cc1004aefeb79daa9cc567630d5d7e1aeb61
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-09-10 08:33:30 +0200

    Add heading to home page

--- apps/expo/src/app/(main)/index.tsx
+++ apps/expo/src/app/(main)/index.tsx
@@ -1,24 +1,56 @@
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
 import { Text } from "~/components/text";
-import Background from "../../../assets/home-bg.svg";
+import { api } from "~/utils/api";
+import BackgroundImage from "../../../assets/home-bg.svg";
 
-export default function Tab() {
+export default function OverviewPage() {
+  const session = api.auth.getSession.useQuery();
+
+  if (session.isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (session.error) {
+    return <Text>Error: {session.error.message}</Text>;
+  }
+
+  if (!session.data) {
+    return <Text>Not authenticated</Text>;
+  }
+
   return (
     <>
       <StatusBar style="light" />
-      <Background
-        width="100%"
-        height={350}
-        preserveAspectRatio="none"
-        style={{
-          position: "absolute",
-        }}
-      />
+      <Background />
       <SafeAreaView>
-        <Text>Tab Settings</Text>
+        <View className="h-4" />
+
+        <Text className="color-white px-8 text-4xl" variant="heading">
+          Moin, {session.data.user?.name}!
+        </Text>
+        <Text className="color-white px-8 text-2xl">Das steht heute an:</Text>
+
+        <View className="h-4" />
+
+        
       </SafeAreaView>
     </>
   );
 }
+
+const Background = () => (
+  <View
+    style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+    }}
+  >
+    <View className="h-16 bg-primary" />
+    <BackgroundImage width="100%" preserveAspectRatio="none" />
+  </View>
+);
