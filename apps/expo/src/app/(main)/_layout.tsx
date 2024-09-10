e271abe424512fc1b0587a1a27e1035f74769e60
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-09-10 08:40:43 +0200

    Remove unnecessary route and redirect

--- apps/expo/src/app/(main)/_layout.tsx
+++ apps/expo/src/app/(main)/_layout.tsx
@@ -1,13 +1,32 @@
-import { Tabs } from "expo-router";
+import { Redirect, Stack, Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/MaterialIcons";
 
 import { colors } from "@stu/tailwind-config/native";
 
 import { Text } from "~/components/text";
+import { useSession } from "~/utils/auth";
 
 export default function TabLayout() {
+  const authenticated = useSession();
+
+  if (authenticated === null) {
+    return <Stack.Screen options={{ headerShown: false }} />;
+  }
+
+  if (authenticated === false) {
+    return <Redirect href="/setup/license-key" />;
+  }
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: colors.primary.text }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary.text,
+        headerTintColor: colors.on.primary,
+        headerStyle: {
+          backgroundColor: colors.primary.DEFAULT,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
