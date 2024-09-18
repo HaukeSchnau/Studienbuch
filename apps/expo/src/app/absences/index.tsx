8f08b4c8173536ad2025b3c8b38999147edb7c42
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-09-18 19:38:25 +0200

    Rename scaffold pages and create new

---
+++ apps/expo/src/app/absences/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { Text } from "~/components/text";
+
+export default function AbsencesPage() {
+  return (
+    <View>
+      <Text>TODO: AbsencesPage</Text>
+    </View>
+  );
+}
