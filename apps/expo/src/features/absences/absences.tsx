c426555395fc223e689b8c1faf894d8f6dca89b8
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-09-18 19:39:13 +0200

    Add absences on home page

---
+++ apps/expo/src/features/absences/absences.tsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { Link } from "expo-router";
+import { formatDate } from "date-fns";
+
+import { colors } from "@stu/tailwind-config/native";
+
+import { PortaledBottomSheet } from "~/components/bottom-sheet";
+import { Button } from "~/components/button";
+import { Card } from "~/components/card";
+import { IconButton } from "~/components/icon-button";
+import { Text } from "~/components/text";
+import { api } from "~/utils/api";
+import { AddAbsence } from "./add-absence";
+import BigCheck from "./big-check.svg";
+import Warning from "./warning.svg";
+
+interface Absence {
+  date: Date;
+  reason: string;
+  course: string;
+  teacherSignature: string | null;
+  parentSignature: string | null;
+}
+
+export const Absences = () => {
+  const [isAddVisible, setIsAddVisible] = useState(false);
+  const onClickAdd = () => setIsAddVisible(true);
+
+  const absences = api.students.absences.listUnexcused.useQuery();
+
+  if (absences.isPending) {
+    return (
+      <Card>
+        <ActivityIndicator />
+      </Card>
+    );
+  }
+
+  if (absences.isError) {
+    return (
+      <Card>
+        <Text>Error: {absences.error.message}</Text>
+      </Card>
+    );
+  }
+
+  return (
+    <Card
+      style={{
+        paddingVertical: 16,
+      }}
+    >
+      <View className="flex-row items-center justify-between">
+        <Text
+          variant="heading"
+          style={{
+            fontSize: 20,
+          }}
+        >
+          Fehlzeiten
+        </Text>
+        <IconButton
+          onPress={onClickAdd}
+          icon="add"
+          size={28}
+          color={colors.primary.text}
+        />
+      </View>
+      <View className="h-2" />
+
+      {absences.data.length === 0 ? (
+        <NoAbsences />
+      ) : (
+        <UnexcusedAbsences absences={absences.data} />
+      )}
+
+      <Link href="/absences" asChild>
+        <Button label="Alle ansehen" className="self-end" />
+      </Link>
+
+      <PortaledBottomSheet onClose={() => setIsAddVisible(false)}>
+        {isAddVisible && <AddAbsence onClose={() => setIsAddVisible(false)} />}
+      </PortaledBottomSheet>
+    </Card>
+  );
+};
+
+const NoAbsences = () => {
+  return (
+    <View className="flex-row">
+      <BigCheck width={40} />
+
+      <View className="w-4" />
+
+      <View className="flex-1">
+        <Text className="opacity-80">
+          Super! All deine Fehlzeiten sind entschuldigt!
+        </Text>
+
+        <View className="h-2" />
+
+        <Text className="opacity-80">
+          Wirst du heute oder morgen fehlen? Dann trage es am besten direkt hier
+          ein.
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const UnexcusedAbsences = ({ absences }: { absences: Absence[] }) => {
+  const numberOfDays = new Set(
+    absences.map((absence) => formatDate(absence.date, "yyyy-MM-dd")),
+  ).size;
+
+  return (
+    <View className="flex-row">
+      <Warning width={40} height={50} />
+
+      <View className="w-4" />
+
+      <View className="flex-1">
+        <Text className="opacity-80">
+          Du hast noch <Text weight="bold">{absences.length}</Text>{" "}
+          unentschuldigte Fehlzeiten an{" "}
+          <Text weight="bold">{numberOfDays}</Text>{" "}
+          {numberOfDays === 1 ? "Tag" : "Tagen"}.
+        </Text>
+      </View>
+    </View>
+  );
+};
