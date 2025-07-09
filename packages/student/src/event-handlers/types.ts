3cdfd9cf5ade217197a23821034d86ff034246f7
Hauke Schnau <hauke@schnau-lilienthal.de>
2025-07-09 18:19:18 +0200

    Start work on schedule page

--- packages/student/src/event-handlers/types.ts
+++ packages/student/src/event-handlers/types.ts
@@ -3,6 +3,7 @@ import type { BaseSQLiteDatabase } from "drizzle-orm/sqlite-core";
 import type * as tables from "../schema";
 
 export type DB = BaseSQLiteDatabase<"sync" | "async", any, typeof tables>;
+
 interface User {
   isOfAge: boolean;
 }
