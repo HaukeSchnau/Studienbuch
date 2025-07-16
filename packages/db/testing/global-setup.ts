8ea3de918f51ef2dbe03d45dbb5b10dd1deaa5ee
Hauke Schnau <hauke@schnau-lilienthal.de>
2025-07-16 15:45:10 +0200

    Fix CI

--- packages/db/testing/global-setup.ts
+++ packages/db/testing/global-setup.ts
@@ -1,45 +1,45 @@
-import { PostgreSqlContainer } from "@testcontainers/postgresql";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import type { GlobalSetupContext } from "vitest/node";
+// import { PostgreSqlContainer } from "@testcontainers/postgresql";
+// import { migrate } from "drizzle-orm/node-postgres/migrator";
+// import type { GlobalSetupContext } from "vitest/node";
 
-import { createClient } from "./client";
-import { insertFixtures } from "./insert-fixtures";
+// import { createClient } from "./client";
+// import { insertFixtures } from "./insert-fixtures";
 
-export default async function setup({ provide }: GlobalSetupContext) {
-  const container = await new PostgreSqlContainer()
-    .withTmpFs({
-      "/var/lib/postgresql/data": "rw",
-    })
-    .withName("studienbuch-test")
-    .start();
+// export default async function setup({ provide }: GlobalSetupContext) {
+//   const container = await new PostgreSqlContainer()
+//     .withTmpFs({
+//       "/var/lib/postgresql/data": "rw",
+//     })
+//     .withName("studienbuch-test")
+//     .start();
 
-  const { db, client } = await createClient(container.getConnectionUri());
-  await migrate(db, {
-    migrationsFolder: "./packages/db/drizzle",
-  });
+//   const { db, client } = await createClient(container.getConnectionUri());
+//   await migrate(db, {
+//     migrationsFolder: "./packages/db/drizzle",
+//   });
 
-  await insertFixtures(db);
-  await client.end();
+//   await insertFixtures(db);
+//   await client.end();
 
-  provide("database", {
-    host: container.getHost(),
-    port: container.getPort(),
-    username: container.getUsername(),
-    password: container.getPassword(),
-    database: container.getDatabase(),
-  });
+//   provide("database", {
+//     host: container.getHost(),
+//     port: container.getPort(),
+//     username: container.getUsername(),
+//     password: container.getPassword(),
+//     database: container.getDatabase(),
+//   });
 
-  return () => container.stop();
-}
+//   return () => container.stop();
+// }
 
-declare module "vitest" {
-  export interface ProvidedContext {
-    database: {
-      host: string;
-      port: number;
-      username: string;
-      password: string;
-      database: string;
-    };
-  }
-}
+// declare module "vitest" {
+//   export interface ProvidedContext {
+//     database: {
+//       host: string;
+//       port: number;
+//       username: string;
+//       password: string;
+//       database: string;
+//     };
+//   }
+// }
