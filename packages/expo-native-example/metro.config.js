d194a1b86f03bebe9607d0c79ee843413ec4f631
Hauke Schnau <hauke@schnau-lilienthal.de>
2025-07-25 15:37:10 +0200

    Fix native modules

--- packages/expo-native-example/metro.config.js
+++ packages/expo-native-example/metro.config.js
@@ -1,12 +1,13 @@
 // metro.config.js
 const path = require("node:path");
 const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
 const withStorybook = require("@storybook/react-native/metro/withStorybook");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withStorybook(config, {
+module.exports = withStorybook(withTurborepoManagedCache(withMonorepoPaths(config)), {
   // Set to false to remove storybook specific options
   // you can also use a env variable to set this
   enabled: true,
@@ -20,3 +21,41 @@ module.exports = withStorybook(config, {
   //   host: 'localhost',
   // },
 });
+
+/**
+ * Add the monorepo paths to the Metro config.
+ * This allows Metro to resolve modules from the monorepo.
+ *
+ * @see https://docs.expo.dev/guides/monorepos/#modify-the-metro-config
+ * @param {import('expo/metro-config').MetroConfig} config
+ * @returns {import('expo/metro-config').MetroConfig}
+ */
+
+function withMonorepoPaths(config) {
+  const projectRoot = __dirname;
+  const workspaceRoot = path.resolve(projectRoot, "../..");
+
+  // #1 - Watch all files in the monorepo
+  config.watchFolders = [workspaceRoot];
+
+  // #2 - Resolve modules within the project's `node_modules` first, then all monorepo modules
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
+  ];
+
+  return config;
+}
+
+/**
+ * Move the Metro cache to the `.cache/metro` folder.
+ * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
+ *
+ * @see https://turbo.build/repo/docs/reference/configuration#env
+ * @param {import('expo/metro-config').MetroConfig} config
+ * @returns {import('expo/metro-config').MetroConfig}
+ */
+function withTurborepoManagedCache(config) {
+  config.cacheStores = [new FileStore({ root: path.join(__dirname, ".cache/metro") })];
+  return config;
+}
