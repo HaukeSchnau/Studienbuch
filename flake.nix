f35142d07797630f88fb88f7e22f6729e71eb43e
Hauke Schnau <hauke@schnau.dev>
2026-05-21 15:50:20 +0200

    fix: declare Android Expo dev toolchain

--- flake.nix
+++ flake.nix
@@ -9,7 +9,26 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+        jdk = pkgs.jdk17;
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          platformVersions = [ "36" ];
+          buildToolsVersions = [
+            "36.0.0"
+            "35.0.0"
+          ];
+          cmakeVersions = [ "3.22.1" ];
+          includeEmulator = true;
+          includeNDK = true;
+          ndkVersions = [ "27.1.12297006" ];
+        };
+        androidSdk = androidComposition.androidsdk;
       in
       {
         devShells.default = pkgs.mkShellNoCC {
@@ -20,8 +39,15 @@
             mprocs
             fastlane
             cocoapods
-            jdk21_headless
+            watchman
+            jdk
+            androidSdk
           ];
+
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk/27.1.12297006";
+          JAVA_HOME = "${jdk.home}";
         };
       }
     );
