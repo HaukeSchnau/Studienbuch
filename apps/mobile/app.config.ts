cecfcedebdb3a5cb55fcae4a4e3dd4c61f78da58
Hauke Schnau <hauke@schnau.dev>
2026-06-10 23:24:50 +0200

    fix: support installable iOS app builds

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -8,6 +8,7 @@ import {
 import { withBuildSourceFile } from "@expo/config-plugins/build/ios/XcodeProjectFile";
 
 const IS_DEV = process.env.APP_VARIANT === "development";
+const appVersion = "1.0.0";
 const iconSuffix = IS_DEV ? "-dev" : "";
 const icon = `./assets/images/icon${iconSuffix}.png`;
 const favicon = `./assets/images/favicon${iconSuffix}.png`;
@@ -284,7 +285,7 @@ export default {
   expo: {
     name: IS_DEV ? "Studienbuch (Dev)" : "Studienbuch",
     slug: "studienbuch",
-    version: "1.0.0",
+    version: appVersion,
     runtimeVersion: {
       policy: "appVersion",
     },
